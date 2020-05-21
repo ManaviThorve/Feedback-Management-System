c45f82ff47d200028d664f21565170ecf2db8df2
Manavi Thorve <manavithorve09@gmail.com>
2020-05-21 13:12:28 +0530

    Add files via upload

---
+++ Feedback/js/first.js
@@ -0,0 +1,9 @@
+function formCreated()
+{
+	alert("FeedBack Form Created Successfully")
+	document.location.href="index.html"
+}
+function addQuestion()
+{
+	document.forms.form1.action="AddQuestion.jsp"
+}
